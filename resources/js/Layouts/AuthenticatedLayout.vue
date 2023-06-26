beb25d1386370b9ef54d6e9d4861e39ffdbf5cb8
dude <betelgeuse1984@outlook.com>
2023-06-26 19:07:05 +0300

    fix

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -8,7 +8,6 @@ import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
 import {Link, usePage} from '@inertiajs/vue3';
 import {Menu, MenuButton, MenuItem, MenuItems} from '@headlessui/vue'
 import {ChevronDownIcon} from '@heroicons/vue/24/solid'
-
 const showingNavigationDropdown = ref(false);
 
 const isBlockCurrent = (routes) => {
@@ -375,12 +374,18 @@ if (role === 0 || role === 1) {
                 <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div class="flex justify-between h-16">
                         <div class="flex">
-                            <div class="shrink-0 flex items-center">
+                            <div class="flex flex-col shrink-0 flex items-center">
                                 <Link :href="route('orders.index')">
                                     <ApplicationLogo
                                         class="block w-auto fill-current text-gray-800"
                                     />
                                 </Link>
+                                <Link
+                                    :href="route('logout')"
+                                    method="post"
+                                    as="button"
+                                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                >Выйти</Link>
                             </div>
 
                             <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
